cd9f4fee06812acb2032759fc7adf2aaef2650b3
Yun Chung <yun.chung@mac.com>
2020-05-13 07:58:23 +0900

    Finished Wiikinimous CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/articles/new', to: 'articles#new'
+  post '/articles', to: 'articles#create'
+
+  get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show'
+
+  get '/articles/:id', to: 'articles#edit'
+  patch '/articles/:id', to: 'articles#update'
+
+  delete '/articles/:id', 'articles#destroy'
 end
